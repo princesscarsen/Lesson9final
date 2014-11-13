1097f68c7ffbce613f098c6ed65bc8564760c395
cars5260 <cars5260@ISS147D8-02.scdsb.pri>
2014-11-13 08:20:30 -0500

    Interface and circle/rect class

--- src/ShapeMaker.java
+++ src/ShapeMaker.java
@@ -1,18 +1,42 @@
+import TerminalIO.KeyboardReader;
 import TurtleGraphics.*;
+import java.awt.Color;
 public class ShapeMaker {
 
     
     public static void main(String[] args) {
-        Shape s;
-        Pen p=new StandardPen();
-        s=new Circle(10,10,5);
-        System.out.println(s);
-        s.move(0, 0);
-        s.stretchBy(2);
-        System.out.println(s);
-        s=new Rect(0,0,50,25);
-        System.out.println(s);
-        s.draw(p);
+        Shape s1=null;
+        Pen p=new StandardPen(new SketchPadWindow(400,500));
+        double r,x,y;
+        double w,h;
+        int choice;
+        KeyboardReader k=new KeyboardReader();
+        while(true){
+            choice=k.readInt("Enter 1 for rectangle, 2 for circle: ");
+            if(choice==1|| choice==2)break;
+        }
+        if(choice==1)
+        {
+            w=k.readDouble("Enter width of rectangle");
+            h=k.readDouble("Enter height of rectangle");
+            s1=new Rect(0,0,w,h);
+            
+        }
+        else
+        {
+            r=k.readDouble("Enter the circles radius");
+            s1=new Circle(0,0,r);
+        }
+        while(true){
+            s1.draw(p);
+            x=k.readDouble("New x");
+            y=k.readDouble("New y");
+            p.setColor(Color.white);
+            s1.draw(p);
+            s1.move(x,y);
+            p.setColor(Color.blue);
+            s1.draw(p);
+        }
         
         }
     
